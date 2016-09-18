8ee39029444cfcfc2e6dc3ac68178a3867ef0138
Dario Vladovic <d.vladimyr@gmail.com>
2016-09-18 18:53:04 +0200

    :fast_forward: Dropping support for older versions of node
    - supporting node@4 and higher
    - renaming download module to packtpub

--- cli.js
+++ cli.js
@@ -2,17 +2,17 @@
 
 'use strict';
 
-var downloadBook = require('./download.js');
+const Promise = require('bluebird');
+const fs = Promise.promisifyAll(require('fs'));
+const pathResolve = require('path').resolve;
+const yargs = require('yargs');
+const ora = require('ora');
+const pkg = require('./package.json');
 
-var Promise = require('bluebird');
-var fs = Promise.promisifyAll(require('fs'));
-var resolve = require('path').resolve;
-var format = require('util').format;
-var yargs = require('yargs');
-var ora = require('ora');
-var pkg = require('./package.json');
+const fetchBook = require('./packtpub.js').fetchBook;
 
-var argv = yargs.version(pkg.version)
+// Setup command line options parsing.
+let argv = yargs.version(pkg.version)
   .usage('\n$0 -c [config file] -t <type> -d <download dir>')
   .option('c', {
     alias: 'config',
@@ -41,26 +41,31 @@ var argv = yargs.version(pkg.version)
   .help('h').alias('h', 'help')
   .argv;
 
-var spinner;
+// Reference to active spinner.
+let spinner;
+
 console.error('\n# Downloading latest free ebook from packtpub.com\n');
 
+// Read credentials.
 readConfig(argv.config)
-  .then(function complete(config) {
-    spinner = ora(format('Logging in using provided credentials [username: %s]', config.username));
+  .then(config => {
+    spinner = ora(`Logging in using provided credentials [username: ${ config.username }]`);
     spinner.start();
 
-    var options = Object.assign({}, config, { type: argv.type });
-    return downloadBook(options);
+    // Log in and fetch book data.
+    let options = Object.assign({}, config, { type: argv.type });
+    return fetchBook(options);
   })
-  .then(function complete(book) {
+  .then(book => {
     spinner.succeed();
 
-    var downloadStream = book.byteStream();
-    downloadStream.on('end', function() { succeed(); });
-    downloadStream.on('error', function(err) { fail(err); });
+    // Download book from remote.
+    let downloadStream = book.byteStream();
+    downloadStream.on('end', () => succeed());
+    downloadStream.on('error', err => fail(err));
     downloadStream.pipe(getOutputStream(book, argv));
   })
-  .error(function onError(err) { fail(spinner, err); });
+  .error(err => fail(err));
 
 function readConfig(path) {
   return fs.readFileAsync(path).then(JSON.parse);
@@ -68,16 +73,16 @@ function readConfig(path) {
 
 function getOutputStream(book, options) {
   if (options.useStdout) {
-    spinner = ora(format('Downloading "%s" [book_id: %s]', book.title, book.id));
+    spinner = ora(`Downloading "${ book.title }" [book_id: ${ book.id }]`);
     spinner.start();
     return process.stdout;
   }
 
-  var filename = format('%s.%s', book.title, argv.type);
-  spinner = ora(format('Writing to "%s.%s" [book_id: %s]', filename, book.id));
+  let filename = `${ book.title }.${ argv.type }`;
+  spinner = ora(`Writing to "${ filename }" [book_id: ${ book.id }]`);
   spinner.start();
 
-  var filepath = resolve(argv.directory || process.cwd(), filename);
+  let filepath = pathResolve(argv.directory || process.cwd(), filename);
   return fs.createWriteStream(filepath);
 }
 
@@ -87,8 +92,8 @@ function succeed(text) {
   console.error();
 }
 
-function fail(error) {
-  spinner.text = format('Error: %s', error.message);
+function fail(err) {
+  spinner.text = `Error: ${ err.message }`;
   spinner.fail();
   console.error();
 }
---
+++ packtpub.js
@@ -0,0 +1,92 @@
+'use strict';
+
+let request = require('request');
+const Promise = require('bluebird');
+const cheerio = require('cheerio');
+const Url = require('url');
+const urlJoin = require('url-join');
+
+request = request.defaults({
+  jar: true,
+  followRedirect: true,
+  followAllRedirects: true
+});
+request = Promise.promisifyAll(request, { multiArgs: true });
+
+const baseUrl = 'https://www.packtpub.com/';
+
+const freeOffersUrl = urlJoin(baseUrl, '/packt/offers/free-learning');
+const ebookDownloadUrl = urlJoin(baseUrl, '/ebook_download/');
+
+module.exports = { fetchBook };
+
+function fetchBook(options) {
+  options = options || {};
+
+  let type = options.type || 'pdf';
+  let username = options.username;
+  let password = options.password;
+
+  let book;
+
+  return request.getAsync(baseUrl)
+    .spread((_, body) => {
+      let form = getFormData(body, username, password);
+      return request.postAsync(baseUrl, { form });
+    })
+    .spread(resp => {
+      if (isRedirected(resp, 'https://www.packtpub.com/')) {
+        return request.getAsync(freeOffersUrl);
+      } else {
+        return Promise.reject(new Promise.OperationalError('Using invalid credentials!'));
+      }
+    })
+    .spread((_, body) => {
+      book = getBookData(body);
+      return request.getAsync(book.claimUrl);
+    })
+    .then(() => {
+      return Object.assign(book, {
+        byteStream() {
+          return request.get(urlJoin(ebookDownloadUrl, book.id, type));
+        }
+      });
+    });
+}
+
+function isRedirected(resp, referer) {
+  return resp.request.headers.referer === referer;
+}
+
+function getFormData(loginPage, username, password) {
+  let $ = cheerio.load(loginPage);
+  let hiddenInputs = 'form#packt-user-login-form input[type="hidden"]';
+
+  let formData = {
+    email: username, password,
+    op: 'Login'
+  };
+
+  $(hiddenInputs).each((i, el) => {
+    let $el = $(el);
+    formData[$el.attr('name')] = $el.attr('value');
+  });
+
+  return formData;
+}
+
+function getBookId(claimUrl) {
+  let path = Url.parse(claimUrl).path;
+  let tokens = path.replace(/^\//, '').split('/');
+  return tokens[1];
+}
+
+function getBookData(offerPage) {
+  let $ = cheerio.load(offerPage);
+
+  let claimUrl = urlJoin(baseUrl, $('[class$="claim"]').attr('href'));
+  let id = getBookId(claimUrl);
+  let title = $('.dotd-title h2').text().trim();
+
+  return { id, title, claimUrl };
+}
